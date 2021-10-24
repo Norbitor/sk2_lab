87da64d9729424f4436680b237e8be7be12aa1b3
Norbert Langner <norbitor@gmail.com>
2021-10-24 14:48:55 +0200

    Example implementation of synchronous server and client

--- linux_sockets/01-simple-tcp-server.c
+++ linux_sockets/01-simple-tcp-server.c
@@ -4,68 +4,124 @@
 #include <sys/socket.h>
 #include <arpa/inet.h>
 #include <unistd.h>
+#include <string.h>
+#include <signal.h>
 
 #define BUFFER_SIZE 512
+#define END_STRING "END."
 
-int main()
+int g_server_socket;
+int g_connection_fd;
+char g_net_buffer[BUFFER_SIZE];
+
+void int_signal_handler()
 {
-    char buff[BUFFER_SIZE];
+    fprintf(stderr, "Gracefully exiting server...\n");
+    if (g_connection_fd != 0)
+        close(g_connection_fd);
+    close(g_server_socket);
+    exit(EXIT_SUCCESS);
+}
 
-    int my_socket = socket(PF_INET, SOCK_STREAM, 0);
-    if (my_socket == -1)
+struct sockaddr_in prepare_address()
+{
+    struct sockaddr_in my_addr;
+    my_addr.sin_family = AF_INET;
+    my_addr.sin_port = htons(1337);
+    my_addr.sin_addr.s_addr = INADDR_ANY;
+    return my_addr;
+}
+
+void server_init()
+{
+    g_server_socket = socket(PF_INET, SOCK_STREAM, 0);
+    if (g_server_socket == -1)
     {
         perror("An error occured while creating a socket");
         exit(EXIT_FAILURE);
     }
 
-    struct sockaddr_in my_addr;
-    my_addr.sin_family = AF_INET;
-    my_addr.sin_port = htons(1337);
-    my_addr.sin_addr.s_addr = INADDR_ANY;
+    struct sockaddr_in my_addr = prepare_address();
 
-    if (bind(my_socket, (struct sockaddr *) &my_addr, sizeof(my_addr)) == -1)
+    if (bind(g_server_socket, (struct sockaddr *)&my_addr, sizeof(my_addr)) == -1)
     {
         perror("An error occured while binding a socket");
-        close(my_socket);
+        close(g_server_socket);
         exit(EXIT_FAILURE);
     }
 
-    if (listen(my_socket, 10) == -1)
+    if (listen(g_server_socket, 10) == -1)
     {
         perror("An error occured while trying to listen");
-        close(my_socket);
+        close(g_server_socket);
         exit(EXIT_FAILURE);
     }
+}
 
-    for(;;)
+void server_loop()
+{
+    socklen_t sock_size = sizeof(struct sockaddr_in);
+    char client_addr[16];
+    char input[BUFFER_SIZE];
+
+    for (;;)
     {
-        int connection_fd = accept(my_socket, NULL, NULL);
-        if (connection_fd == -1)
+        struct sockaddr_in client_info;
+        g_connection_fd = accept(g_server_socket, NULL, NULL);
+        if (g_connection_fd == -1)
         {
             perror("Accept failed");
-            close(my_socket);
+            close(g_server_socket);
             exit(EXIT_FAILURE);
         }
-        puts("Hello to new client");
-        write(connection_fd, "HELLO!", 7);
+        getpeername(g_connection_fd, (struct sockaddr *)&client_info, &sock_size);
+        inet_ntop(AF_INET, &(client_info.sin_addr), client_addr, INET_ADDRSTRLEN);
+        printf("# A new client from %s came in! Conversation started.\n", client_addr);
 
-        int read_bytes;
-        puts("Reading from new client");
-        read_bytes = read(connection_fd, buff, BUFFER_SIZE);
-        printf("Read from socket (size %d): %s\n", read_bytes, buff);
+        for (;;)
+        {
+            memset(g_net_buffer, 0, BUFFER_SIZE);
+            int read_bytes = read(g_connection_fd, g_net_buffer, BUFFER_SIZE);
+            if (read_bytes == 0)
+            {
+                break;
+            }
+            printf("CLIENT> %s\n", g_net_buffer);
+            if (strcmp(g_net_buffer, END_STRING) == 0)
+            {
+                break;
+            }
 
-        puts("Responding");
-        write(connection_fd, "THANKS!", 8);
-
-        if (shutdown(connection_fd, SHUT_RDWR) == -1) {
-            perror("shutdown failed");
-            close(connection_fd);
-            close(my_socket);
-            exit(EXIT_FAILURE);
+            printf("SERVER> ");
+            fgets(input, BUFFER_SIZE, stdin);
+            input[strcspn(input, "\n")] = 0;
+            write(g_connection_fd, input, strlen(input) + 1);
+            if (strcmp(input, END_STRING) == 0)
+            {
+                break;
+            }
         }
-        close(connection_fd);
+        puts("# This conversation has ended. Waiting for a new adventurer!");
+
+        if (shutdown(g_connection_fd, SHUT_RDWR) == -1)
+        {
+            close(g_connection_fd);
+        }
+        close(g_connection_fd);
+        g_connection_fd = 0;
     }
+}
 
-    close(my_socket);
+int main()
+{
+    signal(SIGINT, int_signal_handler);
+    signal(SIGTERM, int_signal_handler);
+
+    memset(g_net_buffer, 0, BUFFER_SIZE);
+
+    server_init();
+    server_loop();
+
+    close(g_server_socket);
     return EXIT_SUCCESS;
 }
--- linux_sockets/02-simple-tcp-client.c
+++ linux_sockets/02-simple-tcp-client.c
@@ -3,12 +3,18 @@
 #include <sys/socket.h>
 #include <arpa/inet.h>
 #include <unistd.h>
+#include <string.h>
 
 #define BUFFER_SIZE 512
 
-int main()
+int main(int argc, char** argv)
 {
+    if (argc < 2)
+    {
+        fprintf(stderr, "You need to specify server IP!\n");
+    }
     char buff[BUFFER_SIZE];
+    char input[BUFFER_SIZE];
 
     int my_socket = socket(PF_INET, SOCK_STREAM, 0);
     if (my_socket == -1)
@@ -20,7 +26,7 @@ int main()
     struct sockaddr_in my_addr;
     my_addr.sin_family = AF_INET;
     my_addr.sin_port = htons(1337);
-    my_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+    my_addr.sin_addr.s_addr = inet_addr(argv[1]);
 
     if(connect(my_socket, (struct sockaddr *) &my_addr, sizeof(my_addr)) == -1)
     {
@@ -28,20 +34,28 @@ int main()
         exit(EXIT_FAILURE);
     }
 
-    int read_bytes = read(my_socket, buff, BUFFER_SIZE);
-    if (read_bytes > 0)
+    for(;;)
     {
-        printf("Message from server: %s\n", buff);
+        printf("CLIENT> ");
+        fgets(input, BUFFER_SIZE, stdin);
+        input[strcspn(input, "\n")] = 0;
+        write(my_socket, input, strlen(input) + 1);
+        if (strcmp(input, "END.") == 0)
+        {
+            break;
+        }
+
+        int read_bytes = read(my_socket, buff, BUFFER_SIZE);
+        if (read_bytes == 0)
+        {
+            break;
+        }
+        printf("SERVER> %s\n", buff);
+        if (strcmp(buff, "END.") == 0)
+        {
+            break;
+        }
     }
-
-    int writed_bytes = write(my_socket, "Message from client", 20);
-
-    read_bytes = read(my_socket, buff, BUFFER_SIZE);
-    if (read_bytes > 0)
-    {
-        printf("Server responded: %s\n", buff);
-    }
-
     close(my_socket);
     return EXIT_SUCCESS;
 }
